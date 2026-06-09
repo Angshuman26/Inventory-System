c6d701c935c559fcd0a080b837352ac8dffb206f
Angshuman Dey <angshumandey2003@gmail.com>
2026-06-09 09:38:53 +0530

    Update API URL to Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // This points exactly to your running FastAPI server
-  baseURL: 'http://127.0.0.1:8000', 
+  baseURL: 'https://inventory-system-okjm.onrender.com', 
 });
 
 export default api;
